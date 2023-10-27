2bc4c1517e9f8a3a858fc19744647f42972e0e7a
Benzigar JS <benzigarjs@tekion.com>
2023-10-27 17:45:22 +0530

    going ong

--- src/organisms/homeSideBar/HomeSideBar.js
+++ src/organisms/homeSideBar/HomeSideBar.js
@@ -3,12 +3,15 @@ import React from "react";
 import VerticalWrapper from "../../atoms/verticalWrapper";
 import SideBarCategory from "../../molecules/sideBarCategory/SideBarCategory";
 
+const getItemsList = (items, selectedCategory, setSelectedCategory = () => {}) =>
+  items?.map((category, i) => (
+    <SideBarCategory category={category} selected={selectedCategory === category.id} setSelected={setSelectedCategory} />
+  ));
+
 export default function HomeSideBar({ items, selectedCategory = false, setSelectedCategory = () => {} }) {
   return (
     <VerticalWrapper className="border-r-2 border-small-width-right w-1/5 overflow-y-scroll pt-5">
-      {items?.map((category, i) => (
-        <SideBarCategory category={category} selected={selectedCategory === category.id} setSelected={setSelectedCategory} />
-      ))}
+      {getItemsList(items, selectedCategory, setSelectedCategory)}
     </VerticalWrapper>
   );
 }

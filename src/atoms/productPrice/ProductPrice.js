88376bc0f14c5690be6d1372a456deac49f578cd
Benzigar JS <benzigarjs@tekion.com>
2023-10-31 16:01:19 +0530

    formatted number

--- src/atoms/productPrice/ProductPrice.js
+++ src/atoms/productPrice/ProductPrice.js
@@ -17,7 +17,7 @@ export default function ProductPrice({
       <div className={classNames("flex items-center", { "font-bold": bold })}>
         {showMinus ? <Text>-</Text> : null}
         <BiRupee className="text-md m-0" />
-        <p>{price}</p>
+        <p>{Intl.NumberFormat("en-IN").format(price)}</p>
       </div>
       {strike ? (
         <div className="absolute top-0 bottom-0 w-full flex justify-center items-center">

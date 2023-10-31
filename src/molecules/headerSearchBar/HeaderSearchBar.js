d0ac30106d0ec3da84b317202b810cb8a3775873
Benzigar JS <benzigarjs@tekion.com>
2023-10-31 17:51:30 +0530

    small changes

--- src/molecules/headerSearchBar/HeaderSearchBar.js
+++ src/molecules/headerSearchBar/HeaderSearchBar.js
@@ -27,7 +27,7 @@ const RenderSearchResult = React.memo(({ searchText = "" }) => {
   return (
     <div className="mt-2 bg-zinc-50 flex flex-col overflow-y-scroll absolute left-0 right-0 rounded-md shadow-xl top-[100%] h-[70vh] p-1 md:p-3">
       <Text className="ml-2 mb-2" bold size="large">
-        Search Results
+        Search Results for "{searchText}"
       </Text>
       {products().length > 0 ? <RenderSearchList products={products()} /> : <SearchEmptyPlaceholder />}
     </div>
